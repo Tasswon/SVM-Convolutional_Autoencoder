c13bb4de95eb39d056bf89a04ff4fdbf2974af1d
Joe Tassone <joetasswon@msn.com>
2019-01-29 18:52:55 -0500

    Update Autoencoder_SVM.py

--- Autoencoder_SVM.py
+++ Autoencoder_SVM.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 # %matplotlib inline
-
 import numpy as np
 import matplotlib.pyplot as plt
 import time
@@ -17,45 +10,31 @@ from sklearn.metrics import accuracy_score
 
 mnist = input_data.read_data_sets("mnist/", one_hot=True)
 
-
-# In[2]:
-
-
 # clears up names and variables
 tf.reset_default_graph()
 
 inputs = tf.placeholder(tf.float32, shape=(None, 28, 28, 1), name='input')
 compare = tf.placeholder(tf.float32, shape=(None, 28, 28, 1), name='compare')
 
-
-# In[3]:
-
-
-#encoder
-
-
-#convolution: input, number of filters, kenerl size, activation function, padding, name
-#maxpool: input, kernel size, stride length, name
-
-#output => 28x28x32
+#########
+# encoder
+#########
+# output => 28x28x32
 convolution_1 = tf.layers.conv2d(inputs, 32, [3,3], activation=tf.nn.relu, padding='same', name='convolution_1')
-#output => 14x14x32
+# output => 14x14x32
 maxpool_1 = tf.layers.max_pooling2d(convolution_1, [2,2], strides=2, name='maxpool_1')
 
-#output => 14x14x64
+# output => 14x14x64
 convolution_2 = tf.layers.conv2d(maxpool_1, 64, [3,3], activation=tf.nn.relu, padding='same', name='convolution_2')
 
-#output => 14x14x32
+# output => 14x14x32
 convolution_3 = tf.layers.conv2d(convolution_2, 32, [3,3], activation=tf.nn.relu, padding='same', name='convolution_3')
-#output => 7x7x32
+# output => 7x7x32
 maxpool_2 = tf.layers.max_pooling2d(convolution_3, [2,2], strides=2, name='maxpool_2')
 
-
-# In[4]:
-
-
+######################################
 # flatten image and feature extraction
-
+######################################
 # 1568
 flatten = tf.layers.flatten(maxpool_2)
 # 128
@@ -63,52 +42,40 @@ dense = tf.layers.dense(flatten, 128, activation=tf.nn.relu)
 # 1568
 unflatten = tf.layers.dense(dense, 1568, activation=tf.nn.relu)
 
-#output => 7x7x32
+# output => 7x7x32
 reshaping = tf.reshape(unflatten, [-1, 7, 7, 32])
 
-
-# In[5]:
-
-
-#decoder
-
-
-#output => 7x7x32
+#########
+# decoder
+#########
+# output => 7x7x32
 convolution_4 = tf.layers.conv2d(reshaping, 32, [3,3], activation=tf.nn.relu, padding='same', name='convolution_4')
-#output => 14x14x32
+# output => 14x14x32
 upsampling_1 = tf.image.resize_nearest_neighbor(convolution_4, [14,14], name='upsampling_1')
 
-#output => 14x14x64
+# output => 14x14x64
 convolution_5 = tf.layers.conv2d(upsampling_1, 64, [3,3], activation=tf.nn.relu, padding='same', name='convolution_5')
 
-#output => 14x14x32
+# output => 14x14x32
 convolution_6 = tf.layers.conv2d(convolution_5, 32, [3,3], activation=tf.nn.relu, padding='same', name='convolution_6')
-#output => 28x28x32
+# output => 28x28x32
 upsampling_2 = tf.image.resize_nearest_neighbor(convolution_6 , [28,28], name='upsampling_2')
 
-#output => 28x28x1
+# output => 28x28x1
 logits = tf.layers.conv2d(upsampling_2, 1, [3,3], activation=None, padding='same')
 
-#reconstructs the image
+# reconstructs the image
 decoder = tf.nn.sigmoid(logits, name='decoder')
 
-
-# In[6]:
-
-
-#calculates cross entropy loss
+# calculates cross entropy loss
 loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=compare, logits=logits)
 loss = tf.reduce_mean(loss)
 
-#define optimizer
+# define optimizer
 optimizer = tf.train.AdamOptimizer(0.001).minimize(loss)
 
 
-# In[7]:
-
-
-#train the autoencoder
-
+# train the autoencoder
 def train_autoencoder():
     epochs = 1
     batches = 20
@@ -132,15 +99,10 @@ def train_autoencoder():
 
         end1 = time.time()
         time1 = end1 - start1
-
         return time1
 
 
-# In[8]:
-
-
-#train the svm
-
+# train the svm
 def train_svm():   
     new_saver = tf.train.Saver()
     start2 = time.time()
@@ -159,15 +121,10 @@ def train_svm():
 
         end2 = time.time()
         time2 = end2 - start2
-        
         return time2, classifier
 
 
-# In[9]:
-
-
 # run the full test of the model
-
 def run_model(classifier):
     new_saver = tf.train.Saver()
 
@@ -182,15 +139,12 @@ def run_model(classifier):
             prediction.append(test)
 
         accuracy = accuracy_score(np.argmax(mnist.test.labels, axis=1), prediction)
-        
         return accuracy 
 
 
-# In[10]:
-
-
+##########################
 # run training and testing
-
+##########################
 sum_time = 0
 sum_accuracy = 0
 count = 1
